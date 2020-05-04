b58a6d8966ab731db0415ff1dd6c8c1c8114c3e0
Aleksandr <yudaev.a@me.com>
2020-05-04 21:20:24 +0300

    lesson

---
+++ react-kabzda-1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+    <div className="App">
+      test text
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </div>
+)
+
+
+export default App;
